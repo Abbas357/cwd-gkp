f36f65bbdefbf03ef56fa381dd3f5ca632b3417b
Abbas357 <abbaskhan357@gmail.com>
2024-11-01 00:50:12 +0500

    Updating migrations

--- database/migrations/2024_10_21_063118_create_activity_log_table.php
+++ database/migrations/2024_10_21_063118_create_activity_log_table.php
@@ -9,22 +9,18 @@ class CreateActivityLogTable extends Migration
     public function up()
     {
         Schema::connection(config('activitylog.database_connection'))->create(config('activitylog.table_name'), function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('log_name', 191)->nullable();
             $table->text('description');
-        
-            $table->string('subject_type', 100);
-            $table->unsignedBigInteger('subject_id');
-            
-            $table->string('causer_type', 100); // Specify length to avoid key length issue
+            $table->string('subject_type', 100)->nullable();
+            $table->unsignedBigInteger('subject_id')->nullable();
+            $table->string('causer_type', 100)->nullable();
             $table->unsignedBigInteger('causer_id')->nullable();
-        
             $table->longText('properties')->nullable();
-            $table->timestamps();
             $table->index(['subject_type', 'subject_id'], 'subject');
             $table->index(['causer_type', 'causer_id'], 'causer');
-        
-            $table->index('log_name');
+            $table->index('log_name')->nullable();
+            $table->timestamps();
         });
         
         
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Site\HomeController as SiteController;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\HomeController as AdminController;
+use App\Http\Controllers\Site\HomeController as SiteController;
 
+Route::get('/dump-autoload', function () {
+    if (app()->environment('local')) { // Restrict to local environment
+        return Artisan::call('composer:dump-autoload');
+    } else {
+        return response('Forbidden', 403);
+    }
+});
+Route::get('/artisan', function () {
+    // \Artisan::call('storage:link');
+    // \Artisan::call('config:cache');
+    Artisan::call('route:cache');
+    Artisan::call('composer:dump-autoload');
+    // \Artisan::call('view:cache');
+    // \Artisan::call('optimize');
+
+    // \Artisan::call('view:clear');
+    // \Artisan::call('route:clear');
+    // \Artisan::call('cache:clear');
+    return 'Command executed successfully.';
+});
 Route::get('/', [SiteController::class, 'site'])->name('site');
 Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('admin.dashboard');
 
