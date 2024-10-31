28170fb5338af93ae7c216870c34a40dc831c17f
Abbas357 <abbaskhan357@gmail.com>
2024-10-31 21:13:31 +0500

    Updating migrations

--- database/migrations/2024_10_21_063118_create_activity_log_table.php
+++ database/migrations/2024_10_21_063118_create_activity_log_table.php
@@ -12,14 +12,21 @@ class CreateActivityLogTable extends Migration
             $table->bigIncrements('id');
             $table->string('log_name', 191)->nullable();
             $table->text('description');
+        
             $table->string('subject_type', 100);
             $table->unsignedBigInteger('subject_id');
-            $table->nullableMorphs('causer', 'causer');
+            
+            $table->string('causer_type', 100); // Specify length to avoid key length issue
+            $table->unsignedBigInteger('causer_id')->nullable();
+        
             $table->longText('properties')->nullable();
             $table->timestamps();
             $table->index(['subject_type', 'subject_id'], 'subject');
+            $table->index(['causer_type', 'causer_id'], 'causer');
+        
             $table->index('log_name');
         });
+        
         
         
     }
