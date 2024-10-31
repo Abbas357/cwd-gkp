f08805443d4442001c8f7099312733c02854dfe1
Abbas Khan <abbaskhan357@gmail.com>
2024-10-31 16:18:57 +0500

    Updating migrations

--- database/migrations/2024_08_26_101153_create_permission_tables.php
+++ database/migrations/2024_08_26_101153_create_permission_tables.php
@@ -25,12 +25,10 @@ return new class extends Migration
         }
 
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
-            //$table->engine('InnoDB');
-            $table->bigIncrements('id'); // permission id
-            $table->string('name');       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
-            $table->string('guard_name'); // For MyISAM use string('guard_name', 25);
+            $table->bigIncrements('id');
+            $table->string('name', 125);
+            $table->string('guard_name', 50);
             $table->timestamps();
-
             $table->unique(['name', 'guard_name']);
         });
 
