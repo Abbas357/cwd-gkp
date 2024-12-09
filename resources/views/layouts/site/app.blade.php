dd15ace208a0f2f9067b48bba3bcb68b8fb080ec
Abbas357 <abbaskhan357@gmail.com>
2024-12-09 12:09:05 +0500

    Changing Loading Indicator

--- resources/views/layouts/site/app.blade.php
+++ resources/views/layouts/site/app.blade.php
@@ -37,6 +37,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600&family=Roboto&display=swap" rel="stylesheet">
 
     <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('site/lib/pace/loading-bar.css') }}">
     <link rel="stylesheet" href="{{ asset('site/css/bootstrap-icons.min.css') }}">
     <link href="{{ asset('site/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('site/css/style.css') }}" rel="stylesheet">
@@ -74,6 +75,7 @@
     @include("layouts.site.partials.footer")
 
     <script src="{{ asset('site/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('site/lib/pace/pace.min.js') }}"></script>
     <script src="{{ asset('site/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('site/js/menu.js') }}"></script>
     <script src="{{ asset('site/js/core.min.js') }}"></script>
--- resources/views/layouts/site/partials/header.blade.php
+++ resources/views/layouts/site/partials/header.blade.php
@@ -1,11 +1,3 @@
-<!-- Spinner Start -->
-<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-        <span class="visually-hidden">Loading...</span>
-    </div>
-</div>
-<!-- Spinner End -->
-
 <header id="integrated-plate" class="CWD">
     <div class="cw-top">
         <div class="cw-top-wrapper">
