ba86ef82a07fead4ff43a85a8b03f54d0ad87463
Abbas Khan <abbaskhan357@gmail.com>
2024-12-19 12:10:41 +0500

    Updating url of old version on new tav

--- resources/views/layouts/site/partials/header.blade.php
+++ resources/views/layouts/site/partials/header.blade.php
@@ -253,7 +253,7 @@
                         </li>
                         <li role="menuitem" aria-hidden="true" class="divider cw-onlyMobileTab"></li>
                         <button class="cw-top-nav-button" style="margin-left: 1.4rem; margin-top: 3px;padding: 7px; border: none; border-radius: 50px">
-                            <a href="https://old.cwd.gkp.pk"><span class="animated-gradient-text">OLD VERSION</span></a>
+                            <a href="https://old.cwd.gkp.pk" target="_blank"><span class="animated-gradient-text">OLD VERSION</span></a>
                         </button>
                     </ul>
                 </nav>
