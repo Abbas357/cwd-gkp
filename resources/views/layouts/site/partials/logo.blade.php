a339e709a1d70b4ac2058fc87190e93a008bc26a
Abbas357 <abbaskhan357@gmail.com>
2024-12-20 09:13:20 +0500

    Updating Logo Code Again

--- resources/views/layouts/site/partials/logo.blade.php
+++ resources/views/layouts/site/partials/logo.blade.php
@@ -1,17 +1,3 @@
-{{-- <div class="cw-logo material">
-    <a class="LogoWrapper dynamic-link" href="{{ route('site') }}" aria-label="Home">
-        <div>
-            <div class="cw-show-DeskTop-Tab">
-                <img class="desktop-logo" style="border-bottom-right-radius: 50px" src="{{ asset('site/images/logo-desktop.gif') }}" alt="" />
-            </div>
-
-            <div class="cw-show-Mobile">
-                <img src="{{ asset('site/images/logo-mobile.png') }}" class="mobile-logo" alt="CWD DEPT" />
-            </div>
-        </div>
-    </a>
-</div> --}}
-
 <div id="cw-logo-container" class="cw-logo material">
 </div>
 
