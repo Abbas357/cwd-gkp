73629200e050c0ec5f5d7897a700b57f0c8327db
Abbas Khan <abbaskhan357@gmail.com>
2025-01-22 16:41:41 +0500

    Footer style changes

--- resources/views/admin/contractors/registration/partials/card.blade.php
+++ resources/views/admin/contractors/registration/partials/card.blade.php
@@ -10,7 +10,7 @@
     }
 
     .contractor-card_front {
-        background-image: url('{{ asset('admin/images/cards/contractor-card-front.png') }}?v=12');
+        background-image: url('{{ asset('admin/images/cards/contractor-card-front.png') }}?v=13');
         background-size: cover;
         background-repeat: no-repeat;
         position: relative;
@@ -157,7 +157,7 @@
     }
 
     .contractor-card_back {
-        background-image: url('{{ asset('admin/images/cards/contractor-card-back.png') }}?v=12');
+        background-image: url('{{ asset('admin/images/cards/contractor-card-back.png') }}?v=13');
         background-size: cover;
         background-repeat: no-repeat;
         position: relative;
