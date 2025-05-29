0d360381f014dfea8a3ba528742cf27c704d967a
Abbas357 <abbaskhan357@gmail.com>
2025-05-29 15:06:21 +0500

    ..........

---
+++ app/View/Components/AssetLayout.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+
+class AssetLayout extends Component
+{
+    public $title;
+    public $showAside;
+
+    public function __construct($title = null, $showAside = true)
+    {
+        $this->title = $title 
+            ? $title .' | ' . setting('site_name', 'main', config('app.name'))
+            : setting('site_name', 'main', config('app.name'));
+        
+        $this->showAside = $showAside;
+    }
+
+    public function render(): View
+    {
+        return view('layouts.apps.assets', ['title' => $this->title, 'showAside' => $this->showAside]);
+    }
+}
